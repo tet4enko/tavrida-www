8847db0bd1c91b34ab79c03b1d6db3dcff9d14e1
Dmitriy Tetchenko <tet4enko@yandex-team.ru>
2020-10-03 01:50:46 +0300

    цены

--- pages/price/index.js
+++ pages/price/index.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import cn from 'classnames';
 import styles from './index.module.scss';
 
 import calc from './pics/Калькулятор доработка.png';
+
+import prices from './prices.json';
 
 const uslugi = [
     [
@@ -46,9 +48,34 @@ const uslugi = [
     ],
 ];
 
+const getTable = (data) => {
+    const getHeaderCell = (cell) => <th colSpan={cell.colspan || '0'} className={cn({ [styles.head]: true })} style={{ width: cell.width || 'unset' }}>{cell.title || cell}</th>;
+    return (
+        <>
+            {data.title && <h3 className={cn({ [styles.tabletitle]: true })}>{data.title}</h3>}
+            <table className={cn({ [styles.table]: true })}>
+                <thead>
+                    {data.header[0] instanceof Array
+                        ? data.header.map((headerRow) => (
+                            <tr>{headerRow.map(getHeaderCell)}</tr>
+                        )) : <tr>{data.header.map(getHeaderCell)}</tr>}
+                </thead>
+                <tbody>
+                    {data.rows.map((row) => (
+                        <tr className={cn({ [styles.row]: true })}>
+                            {row.map((cell, cellIndex) => (
+                                <td className={cn({ [styles.cell]: true })}>{`${cell.title || cell}${cellIndex && data.rubles ? ' р.' : ''}`}</td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
+};
+
 const component = () => (
     <div className={cn({ [styles.Price]: true })}>
-        <img src={calc} className={cn({ [styles.calc]: true })} />
         {uslugi.map((line, i) => (
             <ul className={cn({ [styles.uslugi]: true, [styles[`line${i}`]]: true })}>
                 {line.map((a) => (
@@ -63,37 +90,19 @@ const component = () => (
                 ))}
             </ul>
         ))}
-        {[...uslugi[0], ...uslugi[1]].map((usluga) => (
+        {prices.map(((section) => (
             <div className={cn({ [styles['price-section']]: true })}>
-                <h2 className={cn({ [styles.title]: true })}>{usluga.text.toUpperCase()}</h2>
-                <table className={cn({ [styles.table]: true })}>
-                    <thead>
-                        <tr>
-                            <th className={cn({ [styles.head]: true })} style={{ width: '30%' }}>ВИД УСЛУГИ</th>
-                            <th className={cn({ [styles.head]: true })} style={{ width: '40%' }}>ПОДРОБНЕЕ</th>
-                            <th className={cn({ [styles.head]: true })} style={{ width: '30%' }}>СТОИМОСТЬ</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className={cn({ [styles.row]: true })}>
-                            <td className={cn({ [styles.cell]: true })}>Лайтбокс</td>
-                            <td className={cn({ [styles.cell]: true })} />
-                            <td className={cn({ [styles.cell]: true })} />
-                        </tr>
-                        <tr className={cn({ [styles.row]: true })}>
-                            <td className={cn({ [styles.cell]: true })}>Баннер</td>
-                            <td className={cn({ [styles.cell]: true })} />
-                            <td className={cn({ [styles.cell]: true })} />
-                        </tr>
-                        <tr className={cn({ [styles.row]: true })}>
-                            <td className={cn({ [styles.cell]: true })}>Пленка</td>
-                            <td className={cn({ [styles.cell]: true })} />
-                            <td className={cn({ [styles.cell]: true })} />
-                        </tr>
-                    </tbody>
-                </table>
+                <h2 className={cn({ [styles.title]: true })}>{section.title}</h2>
+                {section.items && section.items.map((item) => (
+                    <>
+                        <h3 className={cn({ [styles.subtitle]: true })}>{item.title}</h3>
+                        {item.tables.map(getTable)}
+                    </>
+                ))}
+                {section.tables && section.tables.map(getTable)}
             </div>
-        ))}
+        )))}
+        <img src={calc} className={cn({ [styles.calc]: true })} />
     </div>
 );
 
