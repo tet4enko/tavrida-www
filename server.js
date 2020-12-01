ef50551999aaa63125876ef234b88aea0da6023a
Dmitriy Tetchenko <tet4enko@yandex-team.ru>
2020-12-01 03:06:42 +0300

    .

---
+++ server.js
@@ -0,0 +1,40 @@
+const { createServer: createServerHTTP } = require('http');
+const { createServer: createServerHTTPS } = require('https');
+const { parse } = require('url');
+const next = require('next');
+const fs = require('fs');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const preparedApp = app.prepare();
+
+if (dev) {
+    preparedApp.then(() => {
+        createServerHTTP((req, res) => {
+            const parsedUrl = parse(req.url, true);
+            handle(req, res, parsedUrl);
+        }).listen(3000, (err) => {
+            if (err) {
+                throw err;
+            }
+        });
+    });
+} else {
+    const options = {
+        key: fs.readFileSync('/etc/certs/ssl.key'),
+        cert: fs.readFileSync('/etc/certs/ssl.crt'),
+        ca: [fs.readFileSync('/etc/certs/root.crt')],
+    };
+    preparedApp.then(() => {
+        createServerHTTPS(options, (req, res) => {
+            const parsedUrl = parse(req.url, true);
+            handle(req, res, parsedUrl);
+        }).listen(443, (err) => {
+            if (err) {
+                throw err;
+            }
+        });
+    });
+}
